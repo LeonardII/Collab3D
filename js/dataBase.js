6cf1adaa1a800f373aab5e39407bdd2ac8c2ac76
Arkan Abdel Qahar Zenal <arkan.zenal@crealogix.com>
2021-02-28 16:51:48 +0100

    r

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,2 +1,6 @@
+
 r = require('rethinkdb')
 
+r.connect ({ host: 'localhost' , port: 3000}, function(err, conn) {      if (err) throw err;      console.log(conn);
+
+    r.db('points').tableCreate('countries').run(counn, function(err ,res){         if(err) throw err;      console.log(res);     })  })
